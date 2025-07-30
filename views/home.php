5ffb517e161512c8d289841b1d74a683ae8aa7d7
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-30 14:06:29 +0530

    added form template

---
+++ views/home.php
@@ -0,0 +1,6 @@
+<h1>Welcome to PHP Web Server</h1>
+<p><a href="/about">About</a></p>
+<form method="POST" action="/submit">
+    <input type="text" name="name" placeholder="Enter your name">
+    <button type="submit">Submit</button>
+</form>
